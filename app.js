6e58e5cd00142fdffac3d27b315221f31ed798d2
Munjurul-Islam <munjurulislam94@gmail.com>
2021-02-08 23:52:36 +0600

    last commit

--- app.js
+++ app.js
@@ -1,55 +1,48 @@
-const searchBtn =  document.getElementById('search_button');
+const searchBtn = document.getElementById('search_button');
 searchBtn.addEventListener('click', () => {
-    
-    let  inputValue = document.getElementById('input-value').value.trim();
-    
-    
-const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${inputValue}`;
+
+    let inputValue = document.getElementById('input-value').value.trim();
+
+
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${inputValue}`;
     fetch(url)
         .then(response => response.json())
         .then(data => {
             // console.log(data.meals[0])
             displayMeals(data.meals)
         })
-        .catch(error => console.log(error)) 
+        .catch(error => console.log(error))
 })
 
 
 const displayMeals = meals => {
     const containerDiv = document.getElementById('foods');
+    containerDiv.innerHTML = '';
     meals.forEach(meal => {
         // console.log(meal)
         const div = document.createElement('div');
         div.className = 'meal';
-        const mealInfo =`
-                <img src='${meal.strMealThumb}' >
+        const mealInfo = `
+                <img onclick="display('${meal.strMeal}')" src='${meal.strMealThumb}' >
                 <h3>${meal.strMeal}</h3>
                 
         `;
         div.innerHTML = mealInfo;
         containerDiv.appendChild(div)
-    });  
+    });
 }
 
-// const details = ingredient => {
-//     const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${ingredient} `
-//     fetch(url)
-//     .then(res => res.json())
-//     .then(data => displayMealIngredient(data))
-    
-// }
-
-const ingredientDetails = document.getElementById('foods');
-ingredientDetails.addEventListener('click', () => {
+const display = ingredientDetails => {
     const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${ingredientDetails} `
     fetch(url)
-    .then(res => res.json())
-    .then(data => displayMealIngredient(data))
-})
+        .then(res => res.json())
+        .then(data => displayMealIngredient(data.meals[0]))
+        .catch(error => console.log(error))
+}
 
 const displayMealIngredient = meal => {
     console.log(meal)
-    
+
     const ingredientDetails = document.getElementById('ingredient-details');
     ingredientDetails.innerHTML = `
         <img src='${meal.strMealThumb}'>
@@ -60,6 +53,11 @@ const displayMealIngredient = meal => {
         <p>${meal.strIngredient3}</p>
         <p>${meal.strIngredient4}</p>
         <p>${meal.strIngredient5}</p>
+        <p>${meal.strIngredient6}</p>
+        <p>${meal.strIngredient7}</p>
+        <p>${meal.strIngredient8}</p>
+        <p>${meal.strIngredient9}</p>
+        <p>${meal.strIngredient10}</p>
         
-    `;      
+    `;
 }
